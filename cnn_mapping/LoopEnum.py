835f1eb508ff227caf6bf5a2ee8b1e2448296638
Mingyu Gao <mgao12@stanford.edu>
2016-11-02 13:19:58 -0700

    [LoopEnum] Extend Operand type enum to loop enum.

---
+++ cnn_mapping/LoopEnum.py
@@ -0,0 +1,15 @@
+'''
+Loop enum type.
+
+Loops include output channel (OC), input channel (IC), output width (OX),
+output height (OY), filter width (FX), filter height (FY).
+'''
+
+OC = 0
+IC = 1
+OX = 2
+OY = 3
+FX = 4
+FY = 5
+NUM = 6
+

050b7e819eef2fa4a9201362083deb0d362491b5
Mingyu Gao <mgao12@stanford.edu>
2016-11-01 22:05:15 -0700

    [CostModel] Define cost function interface.

---
+++ cnn_mapping/CostModel.py
@@ -0,0 +1,12 @@
+'''
+Cost model.
+'''
+
+def get_cost(resource, point):
+    '''
+    Get the cost of the given mapping point on given resource.
+
+    If the point is not feasible on the resource, return inf.
+    '''
+    raise NotImplementedError
+

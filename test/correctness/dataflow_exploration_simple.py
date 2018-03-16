bbf0d6110eab5dfb3efef3d8493ff19f3f0c97ae
xuanyoya <xuany@stanford.edu>
2018-03-15 17:10:15 -0700

    modify dataflow explore test

--- test/correctness/dataflow_exploration_simple.py
+++ test/correctness/dataflow_exploration_simple.py
@@ -30,8 +30,8 @@ class TestDataflow(unittest.TestCase):
 
 
     def test_dataflow_explore(self):
-        capacity_list = [512, 131072, 2097152]
-        access_cost_list = [1, 6, 64]
+        capacity_list = [32, 131072, 2097152]
+        access_cost_list = [0.1, 6, 64]
         static_cost_list = [0.2, 32*0.2, 4096*0.2]
         para_count_list = [12, 1, 1]
 
@@ -39,7 +39,7 @@ class TestDataflow(unittest.TestCase):
         layer = cm.Layer(32, 32, 16, 16, 3, 3, 4)
    
         dataflow_tb = cm.mapping_point_generator.dataflow_exploration(resource, layer, True)
-        print "lala:", dataflow_tb
+        print "dataflows:", dataflow_tb
 
 if __name__ == '__main__':
     unittest.main()
